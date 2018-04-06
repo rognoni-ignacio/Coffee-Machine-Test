7f5fb97cff88c762bea1caf3ddf8bf69db583f1f
Ignacio Rognoni <rognoni.ignacio@gmail.com>
2018-04-06 12:08:11 +0200

    Created layered structure for solution. Added the Entities project.

---
+++ CoffeeMachine/Entities/Coffee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Entities
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public CoffeeType Type { get; set; }
+        public int SpoonsOfSugar { get; set; }
+        public bool UseOwnMug { get; set; }
+
+        public Coffee(CoffeeType type, int spoonsOfSugar, bool useOwnMug)
+        {
+            this.Type = type;
+            this.SpoonsOfSugar = spoonsOfSugar;
+            this.UseOwnMug = useOwnMug;
+        }
+
+        public Coffee()
+        {
+        }
+    }
+
+    public enum CoffeeType
+    {
+        Cafe,
+        The,
+        Chocolat
+    }
+}
